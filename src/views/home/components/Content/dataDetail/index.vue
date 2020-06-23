520d53583bdbacd648d8b3907100a54dea131f07
wb-xt536108 <wb-xt536108@alibaba-inc.com>
2020-06-23 18:48:15 +0800

    Ui调整

--- src/views/home/components/Content/dataDetail/index.vue
+++ src/views/home/components/Content/dataDetail/index.vue
@@ -2,26 +2,29 @@
   <div class="dataDetail-wrap">
     <header>
       <h1>{{detail.title}}</h1>
+      <h2>
+        <span>{{transformTime(detail.createdAt)}}</span>
+        <strong>{{detail.author}}</strong>
+        <i>{{detail.likes}}</i>
+      </h2>
       <article>
         <el-image
-          style="width: 100px; height: 100px"
-          src='https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg'
+          :src="detail.avatar"
           fit="fill">
         </el-image>
-
       </article>
       <footer>
       </footer>
     </header>
-    <article>
+    <article class="dataDetail-content">
       <el-tag>标签一</el-tag>
-      <h2>这是内容</h2>
+      <h2>{{detail.desc}}</h2>
     </article>
     <aside>
       下载
       打赏
     </aside>
-    <footer id="common" style="border: 1px solid #ccc" ref="footer">
+    <footer id="common" class="dataDetail-footer" ref="footer">
       评论
     </footer>
   </div>
@@ -30,6 +33,7 @@
 <script>
 import { getDetail } from '@/service'
 import Valine from 'valine'
+import { transformTime } from '@/utils/common'
 
 export default {
   name: 'dataDetail',
@@ -62,6 +66,11 @@ export default {
       detail: {}
     }
   },
+  computed: {
+    transformTime () {
+      return transformTime
+    }
+  },
   methods: {
     init (id) {
       getDetail(id).then(res => {
@@ -78,9 +87,26 @@ export default {
 .dataDetail-wrap{
   header{
     text-align: center;
-    footer{
-      text-align: center;
+    font-family: PingFangSC-Regular;
+    font-size: 16px;
+  }
+  article{
+    margin-top: 20px;
+    text-align: center;
+    .el-image{
+      margin: 0 auto;
+      width: 100px;
+      height: 100px;
     }
+  }
+  .dataDetail-content{
+    text-align: left;
+    font-family: PingFangSC-Regular;
+    line-height: 20px;
+  }
+  .dataDetail-footer{
+    margin-top: 20px;
+    border: 1px dashed #ccc;
   }
 }
 </style>
